9ebd29c770e779fc7aba3733e5bd7ee126419f39
DiegoIpaez <diegokpo1313@gmail.com>
2023-01-09 19:47:17 -0300

    deploy script added

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:diegoipaez/rickAndMorty_graphql.git main:gh-pages
+# git push -f git@github.com:diegoipaez/rick-and-morty_graphql.git main:gh-pages
